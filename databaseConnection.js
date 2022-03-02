4393bc026056cb20e17de4f4c46f73d450612535
Aditya Gusain <adisandi3112002@gmail.com>
2022-03-02 20:40:38 +0530

    Created local mongodb connection

---
+++ databaseConnection.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+function DbConnect() {
+  const DB_URL = process.env.DB_URL;
+  // dabase connection
+  mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log("Db Connected ...");
+  });
+}
+
+module.exports = DbConnect;
